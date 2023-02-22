2171a055fd45dd2d4400b6e47a461f860ca2100b
Sivov Daniil <sivov1309@gmail.com>
2023-02-22 22:02:25 +0300

    v1.2

--- menu_ex/app/templatetags/tree_menu.py
+++ menu_ex/app/templatetags/tree_menu.py
@@ -1,7 +1,6 @@
 import re
 
 from django import template
-from django.http import HttpRequest
 from django.template import RequestContext
 from django.urls import reverse, NoReverseMatch
 
@@ -11,26 +10,11 @@ register = template.Library()
 
 
 @register.inclusion_tag('app/menu.html', takes_context=True)
-def draw_menu(context: RequestContext, name: str = '', parent: int = 0):
-    """
-    Draw tree menu
-    :param context:
-    :type context: RequestContext
-    :param name:
-    :type name: str
-    :param parent:
-    :type parent: int
-    :return:
-    """
-    #print(context)
+def draw_menu(context: RequestContext, parent: int = 0):
     if parent != 0 and 'menu' in context:
         menu = context['menu']
     else:
         is_url = re.compile(r'^http[s]?://')
-        # Get path if request exist
-        current_path = context['request'].path \
-            if 'request' in context and isinstance(context['request'], HttpRequest) \
-            else ''
 
         data = Menu.objects.all()
 
